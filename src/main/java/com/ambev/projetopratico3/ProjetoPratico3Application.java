492063d6d7d5feb294800ac30cf395af0757f7c2
Michaella de Almeida <michaella.dealmeida@ab-inbev.com>
2023-10-25 18:10:02 -0300

    teste

--- src/main/java/com/ambev/projetopratico3/ProjetoPratico3Application.java
+++ src/main/java/com/ambev/projetopratico3/ProjetoPratico3Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProjetoPratico3Application {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoPratico3Application.class, args);
 	}
